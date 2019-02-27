96d0ae744d66172b446df25fb94bfe1a75c2f6f8
wdd19950122 <38315050+wdd19950122@users.noreply.github.com>
2019-02-27 09:45:44 +0800

    添加get和set方法

--- MyGit/src/com/dsgk/Git01.java
+++ MyGit/src/com/dsgk/Git01.java
@@ -3,4 +3,19 @@ package com.dsgk;
 public class Git01 {
 	private String name;
 	private String pwd;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	
+	
+	
 }
